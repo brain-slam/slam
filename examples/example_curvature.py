ac0f9031939d71a3e876ba6f516ee6c0ee6712ef
toz <b>
2019-11-12 18:57:59 +0100

    pep8 following pull request

--- examples/example_curvature.py
+++ examples/example_curvature.py
@@ -1,36 +1,21 @@
 import trimesh
-import numpy as np
+import slam.plot as splt
 import slam.curvature as get_curvatures
 
 if __name__ == '__main__':
-	
-	# Generate a sphere
-	mesh = trimesh.creation.icosphere()
-	
-	# Show th sphere
-	mesh.show()
-	
-	# Calculate Rusinkiewicz estimation of mean and gauss curvatures
-	PrincipalCurvatures, PrincipalDir1, PrincipalDir2 = get_curvatures.getcurvaturesandderivatives(mesh)
-	gaussian_curv = PrincipalCurvatures[0, :] * PrincipalCurvatures[1, :]
-	mean_curv = 0.5 * (PrincipalCurvatures[0, :] + PrincipalCurvatures[1, :])
-	
-	# Plot mean curvature
-	vect_col_map = \
-		trimesh.visual.color.interpolate(mean_curv, color_map='jet')
-	
-	if mean_curv.shape[0] == mesh.vertices.shape[0]:
-		mesh.visual.vertex_colors = vect_col_map
-	elif mean_curv.shape[0] == mesh.faces.shape[0]:
-		mesh.visual.face_colors = vect_col_map
-	mesh.show( background=[0, 0, 0, 255])
-	
-	# PLot Gauss curvature
-	vect_col_map = \
-		trimesh.visual.color.interpolate(gaussian_curv, color_map='jet')
-	if gaussian_curv.shape[0] == mesh.vertices.shape[0]:
-		mesh.visual.vertex_colors = vect_col_map
-	elif gaussian_curv.shape[0] == mesh.faces.shape[0]:
-		mesh.visual.face_colors = vect_col_map
-	mesh.show(background=[0, 0, 0, 255])
+    # Generate a sphere
+    mesh = trimesh.creation.icosphere()
 
+    # Show th sphere
+    mesh.show()
+
+    # Calculate Rusinkiewicz estimation of mean and gauss curvatures
+    PrincipalCurvatures, PrincipalDir1, PrincipalDir2 = \
+        get_curvatures.getcurvaturesandderivatives(mesh)
+    gaussian_curv = PrincipalCurvatures[0, :] * PrincipalCurvatures[1, :]
+    mean_curv = 0.5 * (PrincipalCurvatures[0, :] + PrincipalCurvatures[1, :])
+
+    # Plot mean curvature
+    splt.pyglet_plot(mesh, mean_curv, plot_colormap=True)
+    # PLot Gauss curvature
+    splt.pyglet_plot(mesh, gaussian_curv, plot_colormap=True)
