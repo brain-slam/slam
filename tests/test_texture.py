c91832589d2019fdfa44bb0bca35859595bc1212
toz <b>
2020-03-11 14:23:56 +0100

    Make install

--- tests/test_texture.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from slam import texture
-
-
-def test_update_darray():
-    test_texture = texture.TextureND()
-    darray = np.zeros((4, 1))
-    test_texture.update_darray(darray)
-    assert test_texture.shape == darray.shape
