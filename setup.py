bb82467d4d8f80aacb870775331827db737375f1
toz <b>
2020-03-12 17:17:12 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="slam",
+    version='0.0.1',
+    packages=find_packages(),
+    author="Guillaume Auzias",
+    description="Surface anaLysis And Modeling",
+    license='MIT',
+    install_requires=["numpy", "trimesh", "matplotlib", "nibabel", "cython", "gdist"]
+)
+
