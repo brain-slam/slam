6125b6ecedfee137e4035e6e364f5511388a04db
Alex <alexandre.pron@gmail.com>
2020-05-26 12:33:01 +0200

    FIX: corrected typo and change gdist channel

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -11,7 +11,8 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 # Create virtual envs with fixed dependencies
-conda create -q -n test-env python=$TRAVIS_PYTHON_VERSION cython numpy matplotlib gdist flake8 autopep8 pytest pytest-cov coveralls
+conda create -q -n test-env python=$TRAVIS_PYTHON_VERSION cython numpy matplotlib flake8 autopep8 pytest pytest-cov coveralls
 conda install -n test-env -c conda-forge nibabel trimesh
-conda activate test_env
+conda  install -n test-env -c mmwoodman gdist
+conda activate test-env
 python ../setup.py install
