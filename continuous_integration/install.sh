4e681383ad125431639a6b371e81719ac8aab40d
Alex <alexandre.pron@gmail.com>
2020-06-02 14:46:41 +0200

    REFATOR : set TRAVIS_PYTHON_VERSION as an env variable in macosX case

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,9 +1,7 @@
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda${TRAVIS_PYTHON_VERSION:0:1}-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-  wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-  #small hack to force the python version to default value tested since travis does not fully support python in macosx
-  TRAVIS_PYTHON_VERSION="3.6"
+elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+  wget http://repo.continuum.io/miniconda/Miniconda${TRAVIS_PYTHON_VERSION:0:1}-latest-MacOSX-x86_64.sh -O miniconda.sh;
 fi
 
 bash miniconda.sh -b -p $HOME/miniconda
@@ -14,7 +12,7 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 # Create virtual envs with fixed dependencies
-conda create -q -n test-env python=$TRAVIS_PYTHON_VERSION cython numpy matplotlib flake8 autopep8 pytest pytest-cov coveralls
+conda create -q -n test-env python=${TRAVIS_PYTHON_VERSION} cython numpy matplotlib flake8 autopep8 pytest pytest-cov coveralls
 conda install -n test-env -c conda-forge nibabel trimesh
 conda activate test-env
 pip install gdist
