0a240da2f22948b12341839bea99684fbda2eaf2
Alex <alexandre.pron@gmail.com>
2020-06-02 14:12:54 +0200

    FIX: corrected travis.yml file to run on macoSx

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -1,7 +1,9 @@
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   wget https://repo.continuum.io/miniconda/Miniconda${TRAVIS_PYTHON_VERSION:0:1}-latest-Linux-x86_64.sh -O miniconda.sh;
 else
-  wget http://repo.continuum.io/miniconda/Miniconda${TRAVIS_PYTHON_VERSION:0:1}-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  #small hack to force the python version to default value tested since travis does not fully support python in macosx
+  TRAVIS_PYTHON_VERSION="3.6"
 fi
 
 bash miniconda.sh -b -p $HOME/miniconda
