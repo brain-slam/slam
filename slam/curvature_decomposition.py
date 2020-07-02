488f1c7c71bae69b079995a536b1fec42255f5b8
anthys <julbarres@gmail.com>
2020-07-02 18:15:48 +0200

    fix_missing_folder

--- slam/curvature_decomposition.py
+++ slam/curvature_decomposition.py
@@ -20,36 +20,3 @@ def decompose_curvature(curvatures):
 def curvedness_shapeIndex(mesh):
     curv = scurv.curvatures_and_derivatives(mesh)[0]
     return decompose_curvature(curv)
-
-
-import slam.io as sio
-import slam.plot as splt
-import slam.curvature as scurv
-
-mesh_a = sio.load_mesh("../examples/data/example_mesh.gii")
-
-
-K = [1, 1]
-quadric = sgps.generate_quadric(K, nstep=20, ax=3, ay=1,
-                                random_sampling=True,
-                                ratio=0.3,
-                                random_distribution_type='gamma')
-quadric_mean_curv = \
-    sgps.quadric_curv_mean(K)(np.array(quadric.vertices[:, 0]),
-                              np.array(quadric.vertices[:, 1]))
-
-#mesh_a = quadric
-
-pcurv = scurv.curvatures_and_derivatives(mesh_a)[0]
-mean_curv = .5 * (pcurv[0, :] + pcurv[1, :])
-shapeIndex, curvedness = decompose_curvature(pcurv)
-
-visb_sc = splt.visbrain_plot(mesh=mesh_a, tex=mean_curv,
-                             caption='mean curvature',)
-
-visb_sc = splt.visbrain_plot(mesh=mesh_a, tex=shapeIndex,
-                             caption='shapeIndex',visb_sc=visb_sc)
-
-visb_sc = splt.visbrain_plot(mesh=mesh_a, tex=curvedness,
-                             caption='curvedness', visb_sc=visb_sc)
-visb_sc.preview()
