5e768976ab591b8654dcb92fb11a8cb4f1093e80
gauzias <guillaume.auzias@univ-amu.fr>
2020-07-02 16:35:54 +0200

    try to fix coveralls bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 BASE_REQUIREMENTS = ['numpy', 'scipy', 'trimesh', 'nibabel', 'networkx']
-TEST_REQUIREMENTS = ['flake8', 'autopep8', 'pytest', 'pytest-cov', 'codecov']
+TEST_REQUIREMENTS = ['flake8', 'autopep8', 'pytest', 'pytest-cov', 'codecov', 'coveralls']
 DIST = ['tvb-gdist@git+https://github.com/the-virtual-brain/tvb-gdist#egg=tvb-gdist']
 VISU = ['visbrain']
 
