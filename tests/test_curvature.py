b8816df853fecdd4d0445db693cf6d0506b98481
anthys <julbarres@gmail.com>
2020-07-08 16:04:45 +0200

    test_curvature_error_drop+gaussian_curve

--- tests/test_curvature.py
+++ tests/test_curvature.py
@@ -27,7 +27,6 @@ class TestCurvatureMethods(unittest.TestCase):
     # Box
     box_A = make_box()
 
-    @unittest.skip
     def test_basic(self):
         mesh_a = self.sphere_A.copy()
         mesh_a_save = self.sphere_A.copy()
@@ -38,7 +37,6 @@ class TestCurvatureMethods(unittest.TestCase):
         assert(mesh_a.vertices == mesh_a_save.vertices).all()
         assert(mesh_a.faces == mesh_a_save.faces).all()
 
-    @unittest.skip
     def test_correctness_curvature(self):
 
         # Iterations on radius
@@ -46,6 +44,7 @@ class TestCurvatureMethods(unittest.TestCase):
         for i in [1, 2, 4]:
 
             precision_A = .000001
+            precision_B = .000001
 
             with self.subTest(i):
 
@@ -57,18 +56,26 @@ class TestCurvatureMethods(unittest.TestCase):
 
                 mean_curv = 0.5*(curv[0, :] + curv[1, :])
 
+                gauss_curv = (curv[0, :] * curv[1, :])
+
                 shape = mean_curv.shape
 
-                # The curvature in every point is 1/radius
-                final = np.full(shape, 1/i)
+                # The mean curvature in every point is 1/radius
+                analytical_mean = np.full(shape, 1/i)
 
-                assert(np.isclose(mean_curv, final, precision_A).all())
+                # The gaussian curvature in every point is 1/radius**2
+                analytical_gauss = np.full(shape, 1/i**2)
+
+
+                assert(np.isclose(mean_curv, analytical_mean, precision_A).all())
+                assert(np.isclose(gauss_curv, analytical_gauss, precision_B).all())
 
         # Iterations on subdivisions
 
         for i in [1, 2, 3]:
 
             precision_A = .000001
+            precision_B = .000001
 
             with self.subTest(i):
 
@@ -78,14 +85,20 @@ class TestCurvatureMethods(unittest.TestCase):
                 curv, d1, d2 = scurv.curvatures_and_derivatives(mesh_a)
 
                 mean_curv = 0.5*(curv[0, :] + curv[1, :])
+                gauss_curv = (curv[0, :] * curv[1, :])
 
                 shape = mean_curv.shape
 
                 # The curvature in every point is 1/radius
-                final = np.full(shape, 1/2)
+                analytical_mean = np.full(shape, 1/2)
 
-                assert(np.isclose(mean_curv, final, precision_A).all())
+                # The curvature in every point is 1/radius**2
+                analytical_gauss = np.full(shape, 1/4)
 
+                assert(np.isclose(mean_curv, analytical_mean, precision_A).all())
+                assert(np.isclose(analytical_gauss, analytical_gauss, precision_B).all())
+
+    @unittest.skip
     def test_correctness_curvature_low_error(self):
 
         K = [1, 1]
@@ -94,17 +107,17 @@ class TestCurvatureMethods(unittest.TestCase):
                                         ratio=0.3,
                                         random_distribution_type='gamma')
 
-        # Computation of estimated curbatures
+        # Computation of estimated curvatures
 
         p_curv, d1, d2 = scurv.curvatures_and_derivatives(quadric)
 
-        k1_estim, k2_estim = p_curv
+        k1_estim, k2_estim = p_curv[0, :], p_curv[1, :]
 
         k_gauss_estim = k1_estim*k2_estim
 
         k_mean_estim = .5*(k1_estim+k2_estim)
 
-        # Computation of analytical curbatures
+        # Computation of analytical curvatures
 
         k_mean_analytic = sgps.quadric_curv_mean(K)(np.array(quadric.vertices[:, 0]),
                                                     np.array(quadric.vertices[:, 1]))
@@ -154,9 +167,9 @@ class TestCurvatureMethods(unittest.TestCase):
 
         # PRINT STATS
         print("----------------------------------------")
-        for i,v in enumerate(a):
+        for i, v in enumerate(a):
             numeric_value = np.round(v[3], decimals=3)
-            if i==6:
+            if i == 6:
                 print("----------------------------------------")
             if len(v) > 4:
                 print('{0:10} {1:5} {2:16} {3:2} {4} {5}'.format(
@@ -165,6 +178,61 @@ class TestCurvatureMethods(unittest.TestCase):
                 print('{0:10} {1:5} {2:16} {3:2} {4}'.format(
                     v[0], v[1], v[2], "=", numeric_value))
         print("----------------------------------------")
+
+    def test_correctness_curvature_drop_error(self):
+
+        out = []
+
+        for j in range(3):
+            K = [1, 1]
+            # Increase the number of points
+            quadric = sgps.generate_quadric(K, nstep=[20+10*j, 20+10*j], ax=3, ay=1,
+                                            random_sampling=True,
+                                            ratio=0.3,
+                                            random_distribution_type='gamma')
+
+            # Computation of estimated curvatures
+
+            p_curv, d1, d2 = scurv.curvatures_and_derivatives(quadric)
+
+            k1_estim, k2_estim = p_curv[0, :], p_curv[1, :]
+
+            k_gauss_estim = k1_estim*k2_estim
+
+            k_mean_estim = .5*(k1_estim+k2_estim)
+
+            # Computation of analytical curvatures
+
+            k_mean_analytic = sgps.quadric_curv_mean(K)(np.array(quadric.vertices[:, 0]),
+                                                        np.array(quadric.vertices[:, 1]))
+
+            k_gauss_analytic = sgps.quadric_curv_gauss(K)(np.array(quadric.vertices[:, 0]),
+                                                          np.array(quadric.vertices[:, 1]))
+
+            k1_analytic = np.zeros((len(k_mean_analytic)))
+            k2_analytic = np.zeros((len(k_mean_analytic)))
+
+            for i in range(len(k_mean_analytic)):
+                a, b = np.roots(
+                    (1, -2*k_mean_analytic[i], k_gauss_analytic[i]))
+                k1_analytic[i] = min(a, b)
+                k2_analytic[i] = max(a, b)
+
+            # /// STATS
+
+            k_mean_relative_change = abs(
+                (k_mean_analytic-k_mean_estim)/k_mean_analytic)
+            k_mean_absolute_change = abs((k_mean_analytic-k_mean_estim))
+
+            k1_relative_change = abs((k1_analytic-k1_estim)/k1_analytic)
+            k1_absolute_change = abs((k1_analytic-k1_estim))
+
+            out += [np.mean(k_mean_absolute_change)]
+
+        print(out)
+
+        # Assert the absolute mean error decreases as we increase the number of points
+        assert(sorted(out, reverse=True) == out)
 
 
 if __name__ == '__main__':
