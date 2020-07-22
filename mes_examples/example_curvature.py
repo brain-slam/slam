7da05cca9513c032f54077b20212e3e13ac2aec3
anthys <julbarres@gmail.com>
2020-07-22 15:19:53 +0200

    add_error_length

--- mes_examples/example_curvature.py
+++ mes_examples/example_curvature.py
@@ -15,6 +15,9 @@ example of curvature estimation in slam
 
 ###############################################################################
 # importation of slam modules
+import slam.utils as ut
+import numpy as np
+import slam.generate_parametric_surfaces as sgps
 import slam.io as sio
 import slam.plot as splt
 import slam.curvature as scurv
@@ -70,4 +73,120 @@ visb_sc = splt.visbrain_plot(mesh=mesh, tex=curvedness,
                              caption='Curvedness',
                              cblabel='Curvedness',
                              cmap='hot')
+visb_sc.preview()
+
+
+###############################################################################
+# Estimation error on the principal curvature length
+K = [1, 0]
+
+quadric = sgps.generate_quadric(
+    K,
+    nstep=[
+        20,
+        20],
+    ax=3,
+    ay=3,
+    random_sampling=False,
+    ratio=0.3,
+    random_distribution_type='gamma', equilateral=True)
+
+###############################################################################
+# Estimated computation of the Principal curvature, K_gauss, K_mean
+p_curv, d1_estim, d2_estim = scurv.curvatures_and_derivatives(
+    quadric)
+
+k1_estim, k2_estim = p_curv[0, :], p_curv[1, :]
+
+k_gauss_estim = k1_estim * k2_estim
+
+k_mean_estim = .5 * (k1_estim + k2_estim)
+
+###############################################################################
+# Analytical computation of the curvatures
+
+k_mean_analytic = sgps.quadric_curv_mean(K)(
+    np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1]))
+
+k_gauss_analytic = sgps.quadric_curv_gauss(K)(
+    np.array(quadric.vertices[:, 0]), np.array(quadric.vertices[:, 1]))
+
+k1_analytic = np.zeros((len(k_mean_analytic)))
+k2_analytic = np.zeros((len(k_mean_analytic)))
+
+for i in range(len(k_mean_analytic)):
+    a, b = np.roots(
+        (1, -2 * k_mean_analytic[i], k_gauss_analytic[i]))
+    k1_analytic[i] = min(a, b)
+    k2_analytic[i] = max(a, b)
+
+
+###############################################################################
+# Error computation
+
+k_mean_relative_change = abs(
+    (k_mean_analytic - k_mean_estim) / k_mean_analytic)
+k_mean_absolute_change = abs((k_mean_analytic - k_mean_estim))
+
+k1_relative_change = abs((k1_analytic - k1_estim) / k1_analytic)
+k1_absolute_change = abs((k1_analytic - k1_estim))
+
+###############################################################################
+# Error plot
+
+visb_sc = splt.visbrain_plot(mesh=quadric, tex=k_mean_absolute_change,
+                             caption='K_mean absolute error',
+                             cblabel='K_mean absolute error',)
+visb_sc.preview()
+
+###############################################################################
+# Estimation error on the curvature directions
+
+
+K = [1, 0]
+
+quadric = sgps.generate_quadric(
+    K,
+    nstep=[
+        20,
+        20],
+    ax=3,
+    ay=3,
+    random_sampling=False,
+    ratio=0.3,
+    random_distribution_type='gamma', equilateral=True)
+
+###############################################################################
+# Estimated computation of the Principal curvature, Direction1, Direction2
+p_curv_estim, d1_estim, d2_estim = scurv.curvatures_and_derivatives(
+    quadric)
+
+###############################################################################
+# Analytical computation of the directions
+analytical_directions = sgps.compute_all_principal_directions_3D(
+    K, quadric.vertices)
+
+estimated_directions = np.zeros(analytical_directions.shape)
+estimated_directions[:, :, 0] = d1_estim
+estimated_directions[:, :, 1] = d2_estim
+
+angular_error_0, dotprods = ut.compare_analytic_estimated_directions(
+    analytical_directions[:, :, 0], estimated_directions)
+angular_error_0 = 180 * angular_error_0 / np.pi
+
+angular_error_1, dotprods = ut.compare_analytic_estimated_directions(
+    analytical_directions[:, :, 1], estimated_directions)
+angular_error_1 = 180 * angular_error_1 / np.pi
+
+###############################################################################
+# Error plot
+
+visb_sc = splt.visbrain_plot(mesh=quadric, tex=angular_error_0,
+                             caption='Angular error 0',
+                             cblabel='Angular error 0',)
+visb_sc.preview()
+
+visb_sc = splt.visbrain_plot(mesh=quadric, tex=angular_error_1,
+                             caption='Angular error 1',
+                             cblabel='Angular error 1',)
 visb_sc.preview()
