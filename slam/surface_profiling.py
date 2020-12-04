00be2567512a48b349d0d131da987fc94af92fb0
Tianqi <tianqisong0117@gmail.com>
2020-12-04 14:42:58 +0100

    update the function select_points_orientation: use the method from trimesh.

--- slam/surface_profiling.py
+++ slam/surface_profiling.py
@@ -1,9 +1,10 @@
 
 import numpy as np
+from scipy.spatial.distance import cdist
 import slam.geodesics
+import slam.utils
 import trimesh
 import trimesh.intersections
-import slam.utils
 
 
 def cortical_surface_profiling(mesh, rot_angle, r_step, max_samples):
@@ -21,7 +22,9 @@ def cortical_surface_profiling(mesh, rot_angle, r_step, max_samples):
     :param max_samples:
         Maximum of samples in one profiles.
 
-    :return:
+    :return: (N_vertex, N_p, N_s, 1) float
+        Profiles points in their 2D coordinates.
+        These points are used to generate the feature Maps.
 
     """
 
@@ -55,13 +58,10 @@ def cortical_surface_profiling(mesh, rot_angle, r_step, max_samples):
             vert_i, vert_norm_i, rot_angle, r_step, max_samples, intersect_mesh)
 
         # compute the 2D coordinates (x, y) for all profile points
-        sample_x, sample_y = compute_profile_coord_x_y(
-            np.array(sam_prof), vert[i], norm[i])
+        sample_x, sample_y = compute_profile_coord_x_y(sam_prof, vert[i], norm[i])
 
         profile_samples_x.append(sample_x)
         profile_samples_y.append(sample_y)
-
-        print(i, 'is done')
 
     return np.array(profile_samples_x), np.array(profile_samples_y)
 
@@ -110,15 +110,15 @@ def surface_profiling_vert(vertex, vert_norm, rot_angle, r_step, max_samples, me
         rot_vec_alpha = np.dot(rot_vec0, rot_mat_alpha)
         p_norm = np.cross(vert_norm, rot_vec_alpha)
 
+        # Get the intersection lines
+        # the lines contains two directions, the rotation direction and reverse.
         intersect_lines = trimesh.intersections.mesh_plane(
             mesh, p_norm, vertex)
 
-        # ori_points = select_points_orientation2(intersect_lines, rot_vec_alpha, vertex, vert_norm)
-        points_i, points_index, _ = select_points_orientation(
-            intersect_lines, rot_vec_alpha, vertex)
+        # select the points in the direction of rotation vector
+        points_i = select_points_orientation(intersect_lines, rot_vec_alpha, vertex, vert_norm)
 
-        # points_i = ori_points
-
+        # Calculate the samples of profiles
         if len(points_i) != 0:
             points_sign = []
             # record the length of segment
@@ -244,128 +244,55 @@ def project_vector2vector(v_n, v_p):
     return v_pn
 
 
-def select_points_orientation2(intersect_points, r_alpha, origin, norm):
+def select_points_orientation(intersect_points, r_alpha, origin, norm):
     """
-     test for
-    :param intersect_points:
-    :param r_alpha:
-    :param origin:
-    :param norm:
-    :return:
+    Select points in a specified orientation.
+
+    :param intersect_points: (n, 2, 3) float
+        points of intersecting lines
+
+    :param r_alpha: (3,) float
+        Orientation vector
+
+    :param origin: (3,) float
+        Origin point
+
+    :param norm: (3,) float
+        Normal of origin point
+
+    :return: r_points, (2n,3) float
+        Ordered points in the orientation,
+        Points are arranged by distance from the center.
+
     """
+
     points_i = intersect_points.reshape(intersect_points.size // 3, 3)
     ordered_points = trimesh.points.radial_sort(points_i, origin, norm)
 
-    # points_i2 = np.copy(points_i)
-    # np.random.shuffle(points_i2)
-    # ordered_points2 = trimesh.points.radial_sort(points_i2, origin, norm)
-
     orientation_vec = np.dot(ordered_points - origin, r_alpha)
-
     orient_point_idx = np.where(orientation_vec > 0)[0][::2]
-
     orient_points = ordered_points[orient_point_idx]
 
+    # find the closest point
+    p2o_len = cdist(orient_points, np.array([origin]), metric='euclidean')
+    p2o_len = p2o_len.reshape(p2o_len.shape[0])
+    ordered_p2o = np.argsort(p2o_len)
+    orient_points = orient_points[ordered_p2o]
+
     return orient_points
-
-
-def select_points_orientation(intersect_points, r_alpha, origin):
-    """
-    Select points in a specified orientation
-
-    :param intersect_points:
-        NOTE: intersecting points of the same polygon are saved together
-
-    :param r_alpha:
-
-    :param origin: (3,) float
-        origin points
-    :return: r_points, points in the direction
-             points_index, points indices
-             line_index, the local indices of the segments that contain the intersecting points.
-    """
-
-    points_i = intersect_points.reshape(intersect_points.size // 3, 3)
-    # points_i = np.array(list(points_i))  # align the array
-
-    # find the center points
-    # p_idx = np.where(points_i == origin)[0]
-    p_idx, count_coord = np.unique(np.where(points_i == origin)[0], return_counts=True)
-    origin_index = p_idx[np.where(count_coord == 3)[0]]
-
-    if len(origin_index) == 0:
-        # the intersection result exclude origin point
-        r_points = []
-        points_index = []
-        line_index = []
-        return r_points, points_index, line_index
-
-    origin_points_i = origin_index // 2  # index in array 'intersect_points'
-
-    # the relative position of origin in intersect_points[i]
-    ori_points_index = origin_index % 2
-    # the relative position of another point in intersect_points[i]
-    another_points_index = (origin_index + 1) % 2
-
-    target_point = points_i[origin_index][0]
-    target_index = origin_index[0]
-
-    # compute the dot between orientation Ra and two initial vector
-    # respectively
-    for i in range(len(origin_index)):
-        point = intersect_points[origin_points_i[i]][another_points_index[i]]
-        origin = intersect_points[origin_points_i[i]][ori_points_index[i]]
-
-        dir_vec = point - origin  # direction vector
-        dot_points = np.dot(dir_vec, r_alpha)
-
-        if dot_points > 0:  # indicate this point in the same orientation
-            target_point = point
-            # get the index in array 'points_i'
-            target_index = origin_points_i[i] * 2 + another_points_index[i]
-            break
-
-    # save the index of points that are in the orientation Ra
-    points_index = [target_index]
-
-    end_bool = False  # =True, if the search process arrives at the end of the orientation Ra
-    while not end_bool:
-
-        tp_idx, tp_count_coord = np.unique(np.where(points_i == target_point)[0], return_counts=True)
-        target_indices = tp_idx[np.where(tp_count_coord == 3)]
-        target_indices = np.unique(np.where(points_i == target_point)[0])
-
-        if len(target_indices) == 1:
-            # only one point in the array, means it is the end point
-            break
-        else:
-            current_index = target_indices[np.where(
-                target_indices != target_index)[0]][0]
-            current_points_i = current_index // 2  # index in array 'intersect_points'
-
-            # the relative position of another point in intersect_points[i]
-            target_points_index = (current_index + 1) % 2
-            # get the index in array 'points_i'
-            target_index = current_points_i * 2 + target_points_index
-            target_point = points_i[target_index]
-
-        if target_index in points_index:
-            break
-        points_index.append(target_index)
-
-    # ordered line index
-    line_index = np.array(points_index) // 2
-
-    r_points = points_i[points_index]
-    return r_points, points_index, line_index
 
 
 def compute_profile_coord_x_y(profile, origin, normal):
     """
     Calculate the 2D coordinates of the profiling points in their rotation planes
-    :param profile:
-    :param origin: center vertex of profiling
-    :param normal:
+
+    :param profile: (N_p, N_s, 3) float
+
+    :param origin: (3,) float
+        Center vertex of profiles
+
+    :param normal: (3,) float
+        Normal of origin
     :return:
     """
     num_prof = len(profile)
