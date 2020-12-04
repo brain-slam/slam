aac9108c5bedf787445553241764485ad5e5eb13
gauzias <guillaume.auzias@univ-amu.fr>
2020-12-04 10:07:18 +0100

    delete examples

--- examples/example_kdtree.py
+++
@@ -1,9 +0,0 @@
-import slam.io as sio
-
-if __name__ == '__main__':
-
-    mesh = sio.load_mesh('data/example_mesh.gii')
-    # kdtree serves to compute distances to mesh vertices efficiently
-    # here we compute the distance between a vector of two points and the mesh
-    distance, index = mesh.kdtree.query([[0., 0., 0.], [0., 0., 1.]])
-    print(distance, index)
