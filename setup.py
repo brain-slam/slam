18ff8263ab8de287bbb4e950c6533a2c2e95f2ae
gauzias <guillaume.auzias@univ-amu.fr>
2020-12-16 18:13:39 +0100

    update the version number to 0.0.3 before uploading to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ VISU = ['visbrain']
 
 setup(
     name="brain-slam",
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     author="Guillaume Auzias",
     description="Surface anaLysis And Modeling",
