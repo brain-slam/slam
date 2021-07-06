3dd8cd6961fc6f288601a408e8ab69f3f3d3f501
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2021-07-06 09:51:05 +0200

    Add example dpf

---
+++ examples/example_dpf_Boucher.py
@@ -0,0 +1,21 @@
+"""
+===================================
+Example of depth potential function in slam
+===================================
+"""
+
+# Authors:
+# Julien Lefevre <julien.lefevre@univ-amu.fr>
+
+# License: BSD (3-clause)
+# sphinx_gallery_thumbnail_number = 2
+
+
+###############################################################################
+# Importation of slam modules
+import slam.distortion as sdst
+import slam.differential_geometry as sdg
+import slam.plot as splt
+import slam.io as sio
+import numpy as np
+
