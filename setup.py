7e227d4807f89fb5c6e206e04c9e1d88665a9e9c
David Meunier <david_meunier_79@hotmail.fr>
2024-11-28 11:28:19 +0100

    rc6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DIST = ["tvb-gdist"]
 
 setup(
     name="brain-slam",
-    version="0.0.4-rc5",
+    version="0.0.4-rc6",
     packages=find_packages(),
     author="Guillaume Auzias",
     description="Surface anaLysis And Modeling",
