4b4394274b722c2b9d9e49d6f439d12cf6def026
GerardMJuan <gerardmartijuan@gmail.com>
2024-11-28 16:47:13 +0100

    testing with updated yml

--- tests/test_surface_profiling.py
+++ tests/test_surface_profiling.py
@@ -13,6 +13,7 @@ from slam.surface_profiling import (
     second_round_profiling_vert,
     vert2poly_indices,
     get_texture_value_on_profile,
+    cortical_surface_profiling
 )
 from slam.texture import TextureND
 
@@ -30,42 +31,41 @@ class TestSurfaceProfiling(unittest.TestCase):
         self.vertex = self.mesh.vertices[self.vertex_idx]
         self.normal = self.mesh.vertex_normals[self.vertex_idx]
 
-    # COMMENTED DUE TO PROBLEMS RELATED TO GDIST
-    # def test_cortical_surface_profiling(self):
-    #     """Test cortical surface profiling functionality.
+    def test_cortical_surface_profiling(self):
+        """Test cortical surface profiling functionality.
 
-    #     Verifies the computation of surface profiles on a unit sphere,
-    #     checking the output shapes and expected coordinates.
-    #     """
-    #     rot_angle = 90.0  # Simpler angle for verification
-    #     r_step = 0.1
-    #     max_samples = 3
+        Verifies the computation of surface profiles on a unit sphere,
+        checking the output shapes and expected coordinates.
+        """
+        rot_angle = 90.0  # Simpler angle for verification
+        r_step = 0.1
+        max_samples = 3
 
-    #     profile_x, profile_y = cortical_surface_profiling(
-    #         self.mesh, rot_angle, r_step, max_samples
-    #     )
+        profile_x, profile_y = cortical_surface_profiling(
+            self.mesh, rot_angle, r_step, max_samples
+        )
 
-    #     # Number of profiles per vertex
-    #     expected_profiles = int(360 / rot_angle)
-    #     n_vertices = len(self.mesh.vertices)
-    #     expected_shape = (n_vertices, expected_profiles, max_samples)
-    #     self.assertEqual(profile_x.shape, expected_shape)
-    #     self.assertEqual(profile_y.shape, expected_shape)
+        # Number of profiles per vertex
+        expected_profiles = int(360 / rot_angle)
+        n_vertices = len(self.mesh.vertices)
+        expected_shape = (n_vertices, expected_profiles, max_samples)
+        self.assertEqual(profile_x.shape, expected_shape)
+        self.assertEqual(profile_y.shape, expected_shape)
 
-    #     # Test specific values for the top vertex (0,1,0)
-    #     top_vertex_idx = np.argmax(self.mesh.vertices[:, 1])
-    #     # For a unit sphere, x coordinates must be roughly r_step multiples
-    #     expected_x_steps = np.array([0.1, 0.2, 0.3])  # r_step multiples
-    #     np.testing.assert_array_almost_equal(
-    #         profile_x[top_vertex_idx, 0, :], expected_x_steps, decimal=1
-    #     )
+        # Test specific values for the top vertex (0,1,0)
+        top_vertex_idx = np.argmax(self.mesh.vertices[:, 1])
+        # For a unit sphere, x coordinates must be roughly r_step multiples
+        expected_x_steps = np.array([0.1, 0.2, 0.3])  # r_step multiples
+        np.testing.assert_array_almost_equal(
+            profile_x[top_vertex_idx, 0, :], expected_x_steps, decimal=1
+        )
 
-    #     # SImilar idea to expected_x_steps, but for y, starting from 0
-    #     # Bit hacky, but should be reproducible
-    #     expected_y = np.array([-0.03, -0.06, -0.09])
-    #     np.testing.assert_array_almost_equal(
-    #         profile_y[top_vertex_idx, 0, :], expected_y, decimal=2
-    #     )
+        # SImilar idea to expected_x_steps, but for y, starting from 0
+        # Bit hacky, but should be reproducible
+        expected_y = np.array([-0.03, -0.06, -0.09])
+        np.testing.assert_array_almost_equal(
+            profile_y[top_vertex_idx, 0, :], expected_y, decimal=2
+        )
 
     def test_surface_profiling_vert(self):
         """Test profiling for a single vertex.
