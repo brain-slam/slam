3fb5de98b9bdc63b75bee201168fa4e9a1500bf0
gauzias <guillaume.auzias@univ-amu.fr>
2025-04-22 21:01:33 +0000

    Deploying to gh-pages from @ brain-slam/slam@73e637a69e27fc4756c1c778ae03b123ad07f803 🚀

---
+++ _downloads/a68cfdf72dfb69b76d2a0c692fe6c3d9/example_sulcal_graph.py
@@ -0,0 +1,64 @@
+"""
+.. _example_sulcal_graph:
+
+===================================
+Example of sulcal graph in slam
+===================================
+"""
+
+# Authors:
+# Lucile Hashimoto lucile-hashimoto
+# Guillaume Auzias <guillaume.auzias@univ-amu.fr>
+
+# License: MIT
+# sphinx_gallery_thumbnail_number = 2
+
+###############################################################################
+# NOTE: there is no visualization tool in slam, but we provide at the
+# end of this script exemplare code to do the visualization with
+# an external solution
+###############################################################################
+
+###############################################################################
+# importation of slam modules
+import slam.io as sio
+import slam.sulcal_graph as ssg
+import numpy as np
+
+###############################################################################
+# loading an examplar mesh and corresponding texture
+path_to_mesh = "../examples/data/example_mesh.gii"
+path_to_texture = "../examples/data/example_texture.gii"
+path_to_mask = ""
+mesh = sio.load_mesh(path_to_mesh)
+side = "left"
+texture = sio.load_texture(path_to_texture)
+dpf = np.array(texture.darray[0])
+
+###############################################################################
+# extract the sulcal graph from a mesh
+g = ssg.extract_sulcal_graph(side, path_to_mesh, path_to_features=None, path_to_output=None, path_to_mask=None)
+
+###############################################################################
+# add an attribute to nodes
+g = ssg.add_node_attribute_to_graph(g, dpf, name='pit_depth', save=False)
+print("Node attributes:\n", g.nodes[0].keys())
+print("First node:\n", g.nodes[0])
+
+###############################################################################
+# add an attribute to edges
+g = ssg.add_edge_attribute_to_graph(g, dpf, name='ridge_depth_bis', save=False)
+print("Edge attributes:\n", g.edges[list(g.edges)[0]].keys())
+print("First edge:\n", g.edges[list(g.edges)[0]])
+
+###############################################################################
+# add geodesic distances attribute to edges
+g = ssg.add_geodesic_distances_to_graph(g, mesh, save=False)
+
+###############################################################################
+# add mean value to nodes attributes
+g = ssg.add_mean_value_to_graph(g, dpf, name='basin_mean_depth', save=False)
+
+###############################################################################
+# get textures from graph
+tex_labels, tex_pits, tex_ridges = ssg.get_textures_from_graph(g, mesh, save=False, outdir=None)
---
+++ _downloads/aa2194478efb712cd62cb2e62b466a9f/example_watershed.py
@@ -0,0 +1,70 @@
+"""
+.. _example_watershed:
+
+===================================
+Example of watershed in slam
+===================================
+"""
+
+# Authors:
+# Lucile Hashimoto lucile-hashimoto
+# Guillaume Auzias <guillaume.auzias@univ-amu.fr>
+
+# License: MIT
+# sphinx_gallery_thumbnail_number = 2
+
+###############################################################################
+# NOTE: there is no visualization tool in slam, but we provide at the
+# end of this script exemplare code to do the visualization with
+# an external solution
+###############################################################################
+
+###############################################################################
+# importation of slam modules
+import slam.io as sio
+import slam.watershed as swat
+import slam.sulcal_graph as ssg
+
+###############################################################################
+# loading an examplar mesh and corresponding texture
+path_to_mesh = "../examples/data/example_mesh.gii"
+path_to_mask = ""
+path_to_output = ""
+
+mesh = sio.load_mesh(path_to_mesh)
+side = "left"
+
+###############################################################################
+# compute curvature, dpf and voronoi
+_, dpf, voronoi = swat.compute_mesh_features(mesh, save=False, outdir=path_to_output, check_if_exist=True)
+
+###############################################################################
+# normalize watershed thresholds
+thresh_dist, thresh_ridge, thresh_area = swat.normalize_thresholds(mesh, voronoi,
+                                                                   thresh_dist=20.0,
+                                                                   thresh_ridge=1.5,
+                                                                   thresh_area=50.0,
+                                                                   side=side)
+
+###############################################################################
+# define the exclusion mask (cingular pole)
+if path_to_mask:
+    mask = sio.load_texture(path_to_mask).darray[0]
+else:
+    mask = None
+
+###############################################################################
+# extract sulcal pits and associated basins
+basins, ridges, adjacency = swat.watershed(mesh, voronoi, dpf, thresh_dist, thresh_ridge, thresh_area, mask)
+
+###############################################################################
+# generate the textures from watershed outputs
+tex_labels, tex_pits, tex_ridges = swat.get_textures_from_dict(mesh, basins, ridges, save=True, outdir=path_to_output)
+
+###############################################################################
+# generate the sulcal graph
+g = ssg.get_sulcal_graph(adjacency, basins, ridges, save=True, outdir=path_to_output)
+
+###############################################################################
+# generate the textures from graph
+tex_labels, tex_pits, tex_ridges = ssg.get_textures_from_graph(g, mesh, save=True, outdir=path_to_output)
