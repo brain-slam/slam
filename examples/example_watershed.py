5333accf104b473b625d3ae15886fcfd998f0bff
gauzias <guillaume.auzias@univ-amu.fr>
2026-01-29 21:46:46 +0100

    [WIP] management of ridge_vertices in watershed

--- examples/example_watershed.py
+++ examples/example_watershed.py
@@ -30,19 +30,23 @@ import slam.sulcal_graph as ssg
 path_to_mesh = "../examples/data/example_mesh.gii"
 path_to_mask = None
 path_to_output = ""
+#path_to_mesh = "/mnt/data/work/BV_database/BV_db_test/subjects/auzias/t1mri/default_acquisition/default_analysis/segmentation/mesh/auzias_Lwhite.gii"
+#path_to_output = "/mnt/data/work/BV_database/BV_db_test/subjects/auzias/t1mri/default_acquisition/default_analysis/segmentation/mesh/"
 
+#path_to_mesh = "/mnt/data/work/python_sandBox/brain_slam/debug_watershed/example_mesh.gii"
+#path_to_output = "/mnt/data/work/python_sandBox/brain_slam/debug_watershed/"
 mesh = sio.load_mesh(path_to_mesh)
 side = "left"
 
 ###############################################################################
 # compute curvature, dpf and voronoi
-_, dpf, voronoi = swat.compute_mesh_features(mesh, save=False, outdir=path_to_output, check_if_exist=True)
+_, dpf, voronoi = swat.compute_mesh_features(mesh, save=True, outdir=path_to_output, check_if_exist=True)
 
 ###############################################################################
 # normalize watershed thresholds
 thresh_dist, thresh_ridge, thresh_area = swat.normalize_thresholds(voronoi, thresh_dist=20.0, thresh_ridge=1.5,
                                                                    thresh_area=50.0, side=side)
-
+thresh_dist, thresh_ridge, thresh_area = 0,0,0
 ###############################################################################
 # define the exclusion mask (cingular pole)
 if path_to_mask is not None:
